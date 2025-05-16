7712156e95a1be6cd3588114103c3328a903dfe8
Kisuke Watanabe <wkisuke0122@icloud.com>
2025-05-16 18:04:57 +0900

    Postgresql update

--- time_project/__init__.py
+++ time_project/__init__.py
@@ -12,7 +12,13 @@ app = Flask(__name__)
 load_dotenv()
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+uri = os.getenv('DATABASE_URL')
+
+# PostgreSQLの旧フォーマット対応（herokuなど）
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 #SQLAlchemyの変更追跡機能の無効化
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -28,20 +34,11 @@ def localize_callback (*args,**kwargs):
     return 'このページにアクセスするには、ログインが必要です。'
 login_manager.localize_callback = localize_callback
 
-from sqlalchemy.engine import Engine
-from sqlalchemy import event
-
 # ユーザーのロード関数（必須）
 @login_manager.user_loader
 def load_user(user_id):
     from time_project.models import User  # models.py から User クラスをインポート
     return User.query.get(int(user_id))
-
-@event.listens_for(Engine,"connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
 
 
 from time_project.main.views import main
